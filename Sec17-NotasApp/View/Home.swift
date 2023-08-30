aa0e609a15a1c5ca8a40da75f3e76fbae9c87d98
Nelson Tapia <ntapiapinto@gmail.com>
2023-08-30 00:43:18 -0400

    Se agrega FetchRequest variable results Se agrega a la vista la nota y fecha

--- Sec17-NotasApp/View/Home.swift
+++ Sec17-NotasApp/View/Home.swift
@@ -4,16 +4,31 @@ import SwiftUI
 struct Home: View {
     
     @StateObject var model = ViewModel()
+    @FetchRequest(entity: Notas.entity(), sortDescriptors: [NSSortDescriptor(key: "fecha", ascending: true)], animation: .spring()) var results: FetchedResults<Notas>
     
     var body: some View {
         
-        Button(action:{
-            model.show.toggle()
-        }){
-            Image(systemName: "plus")
-        }.sheet(isPresented: $model.show) {
-            AddView(model: model)
+        NavigationView {
+            List{
+                ForEach(results){ item in
+                    VStack(alignment: .leading){
+                        Text(item.nota ?? "Sin Notas")
+                            .font(.title)
+                            .bold()
+                        Text(item.fecha ?? Date(), style: .date)
+                    }
+                }
+            }.navigationTitle("Notas")
+                .navigationBarItems(trailing:
+                                        Button(action:{
+                    model.show.toggle()
+                }){
+                    Image(systemName: "plus").font(.largeTitle).foregroundColor(.blue)
+                }
+                ).sheet(isPresented: $model.show) {
+                    AddView(model: model)
+                }
         }
-    }
+    }//Body
 }
 
